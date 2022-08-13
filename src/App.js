817c2ae4c93d2c3957873a244d3d455b351da538
Li-Haowei <haoweilee666@gmail.com>
2022-08-12 20:10:12 -0400

    summor search finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [searchText, setSearchText] = useState("");
+    const [playerData, setPlayerData] = useState({});
+    const API_KEY = "RGAPI-f720f147-b0aa-433d-9830-bd83ab687e82";
+    
+    function searchForPlayer(event){
+        //Set up the correct API call
+        var APICallString = "https://na1.api.riotgames.com" + "/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + API_KEY
+        //handle API call
+        axios.get(APICallString).then(function (response){
+            //Success
+            setPlayerData(response.data)
+        }).catch(function (error){
+            //Error
+            console.log(error)
+        })
+    }
+    //console.log(playerData)
+    return ( 
+    <div className = "App" >
+        <div className = "container">
+            <h5>League Of Legends</h5>
+            <input type={'text'} onChange={ e => setSearchText(e.target.value)}></input>
+            <button onClick={e => searchForPlayer(e)}>Search</button>
+        </div>
+        {JSON.stringify(playerData) != '{}' ? 
+        <>
+            <p>{playerData.name}</p>
+            <p>Summoner Level {playerData.summonerLevel}</p>
+            <img width="200px" height="200px" src={"http://ddragon.leagueoflegends.com/cdn/12.15.1/img/profileicon/" + playerData.profileIconId +".png"}></img>
+        </> 
+        : 
+        <><p>No Player Data</p></>
+        }
     </div>
-  );
+    );
 }
 
 export default App;
