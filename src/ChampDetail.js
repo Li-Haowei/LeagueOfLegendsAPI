e0b7c92182cec47c78d0547b699599542cb4bfd2
Haowei Li <91715036+Li-Haowei@users.noreply.github.com>
2022-08-22 01:11:33 -0400

    trying to call function when ready

--- src/ChampDetail.js
+++ src/ChampDetail.js
@@ -6,6 +6,8 @@ import { useSearchParams  } from 'react-router-dom'
 function ChampDetail(){
     const [searchParams, setSearchParams] = useSearchParams();
     const [LoLCurrentVersion, setLoLCurrentVersion] = useState("12.14.1");
+    const [champDetails, setChampDetails] = useState({});
+    //var champDetails = {};
     const API_KEY = process.env.REACT_APP_API_KEY;
     const championName = searchParams.get("championName")
     axios.get("https://ddragon.leagueoflegends.com/api/versions.json").then(
@@ -17,19 +19,27 @@ function ChampDetail(){
                  fetch('http://ddragon.leagueoflegends.com/cdn/' 
                  + LoLCurrentVersion + '/data/en_US/champion/' + championName + '.json').then((response) => response.json())
                                         .then((responseJson) => {
-                                            
-                                            console.log(responseJson.data)
-                                            //console.log(myChamps[responseJson.data[i]['key']])
+                                            setChampDetails(responseJson.data[championName]);
+                                            console.log(champDetails);
+                                            console.log(champDetails['lore'])
                                         })
-                                    }, 0);
+                                    }, 500);
         }
-    
+    //console.log(JSON.stringify(champDetails) !== '{}')
 
-        searchForChampion();
+        //searchForChampion();
     return (
         <div className="main-page">
+            <script>document.ready(searchForChampion())</script>
             <h1>{championName} Details</h1>
             <img src={"http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+ championName +"_0.jpg"}></img>
+            {JSON.stringify(champDetails) !== '{}' ? 
+            <>  
+            <p>{champDetails['lore']}</p>
+            </> 
+            : 
+            <><p>Loading</p></>
+            }
         </div>
     )
 }
