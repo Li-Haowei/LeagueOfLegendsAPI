08ac382e812e942354ba23fc8b8f58f25330e672
Li-Haowei <haoweilee666@gmail.com>
2022-08-22 23:07:10 -0400

    champion spell details

--- src/ChampDetail.js
+++ src/ChampDetail.js
@@ -20,22 +20,60 @@ function ChampDetail(){
                  + LoLCurrentVersion + '/data/en_US/champion/' + championName + '.json').then((response) => response.json())
                                         .then((responseJson) => {
                                             setChampDetails(responseJson.data[championName]);
-                                            //console.log(champDetails);
                                             //console.log(champDetails['lore'])
                                         })
                                     }, 500);
         }
     //console.log(JSON.stringify(champDetails) !== '{}')
-
+    console.log(champDetails);
         //searchForChampion();
+
+    
     return (
         <div className="main-page">
-            <script>{searchForChampion()}</script>
+            {JSON.stringify(champDetails) !== '{}' ? 
+            <>  
+            </> 
+            : 
+            <><script>{searchForChampion()}</script></>
+            }
             <h1>{championName} Details</h1>
             <img src={"http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+ championName +"_0.jpg"}></img>
             {JSON.stringify(champDetails) !== '{}' ? 
             <>  
             <p className='background-info'>{champDetails['lore']}</p>
+            <table>
+            <tbody>
+            <tr>
+                <th>Passive</th>
+                <th><img src={"http://ddragon.leagueoflegends.com/cdn/" + LoLCurrentVersion + "/img/passive/"+ champDetails['passive']['image']['full']}></img></th>
+                <th>{champDetails['passive']['description']}</th>
+            </tr>
+            <tr>
+                <th>{champDetails['spells'][0]['name']}</th>
+                <th><img src={"http://ddragon.leagueoflegends.com/cdn/" + LoLCurrentVersion + "/img/spell/"+ champDetails['spells'][0]['id'] +".png"}></img></th>
+                <th>
+                    {champDetails['spells'][0]['description'].split('<br>')} 
+
+                 Cooldown: {champDetails['spells'][0]['cooldownBurn']} <br></br> Cost: {champDetails['spells'][0]['costBurn']} </th>
+            </tr>
+            <tr>
+                <th>{champDetails['spells'][1]['name']}</th>
+                <th><img src={"http://ddragon.leagueoflegends.com/cdn/" + LoLCurrentVersion + "/img/spell/"+ champDetails['spells'][1]['id'] +".png"}></img></th>
+                <th>{champDetails['spells'][1]['description'].replaceAll('<br>', '\n')} <br></br> Cooldown: {champDetails['spells'][1]['cooldownBurn']} <br></br> Cost: {champDetails['spells'][1]['costBurn']} </th>
+            </tr>
+            <tr>
+                <th>{champDetails['spells'][2]['name']}</th>
+                <th><img src={"http://ddragon.leagueoflegends.com/cdn/" + LoLCurrentVersion + "/img/spell/"+ champDetails['spells'][2]['id'] +".png"}></img></th>
+                <th>{champDetails['spells'][2]['description'].replaceAll('<br>', '\n')} <br></br> Cooldown: {champDetails['spells'][2]['cooldownBurn']} <br></br> Cost: {champDetails['spells'][2]['costBurn']} </th>
+            </tr>
+            <tr>
+                <th>{champDetails['spells'][3]['name']}</th>
+                <th><img src={"http://ddragon.leagueoflegends.com/cdn/" + LoLCurrentVersion + "/img/spell/"+ champDetails['spells'][3]['id'] +".png"}></img></th>
+                <th>{champDetails['spells'][3]['description'].replaceAll('<br>', '\n')} <br></br> Cooldown: {champDetails['spells'][3]['cooldownBurn']} <br></br> Cost: {champDetails['spells'][3]['costBurn']}</th>
+            </tr>
+            </tbody>
+            </table>
             </> 
             : 
             <><p>Loading</p></>
